dd78758224798bfcf72c03b360245bad8d49a4b6
An2nJohansson <an2n_johansson@outlook.com>
2025-01-30 09:10:57 +0100

    Add project files.

---
+++ TicketBookingCore.Tests/TicketBookingRequestProcessorTests.cs
@@ -0,0 +1,45 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TicketBookingCore.Tests
+{
+    public class TicketBookingRequestProcessorTests
+    {
+        private readonly TicketBookingRequestProcessor _processor;
+        public TicketBookingRequestProcessorTests() 
+        {
+            _processor = new TicketBookingRequestProcessor();
+        }
+
+        [Fact]
+        public void ShouldReturnTicketBookingResultWithRequestValues()
+        {
+            
+            var request = new TicketBookingRequest
+            {
+                FirstName = "Anton",
+                LastName = "Johansson",
+                Email = "anton@gmail.se"
+            };
+
+            // Act
+            TicketBookingResponse response = _processor.Book(request);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.Equal(request.FirstName, response.FirstName);
+            Assert.Equal(request.LastName, response.LastName);
+            Assert.Equal(request.Email, response.Email);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionIfRequestIsNull()
+        {
+           
+            // Act
+            var exception = Assert.Throws<ArgumentNullException>(() => _processor.Book(null));
+
+            // Assert
+            Assert.Equal("request", exception.ParamName);
+        }
+    }
+}
